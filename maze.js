222e04a14fd4125a0922d7b63c086ae6625e5225
kuantumleap123 <k3v_w1l@yahoo.com>
2019-12-01 20:01:03 +0700

    show solution, randomization attempt to get shorter path

--- maze.js
+++ maze.js
@@ -4,7 +4,7 @@ class Maze
     {
         this.size = size;
         this.grid = this.createMaze(size);
-        
+        this.solution = [];
     }
 
     createMaze(size)
@@ -90,10 +90,50 @@ class Maze
 
         if(this.grid[pRow] == undefined || this.grid[pRow][pCol] == undefined || this.grid[pRow][pCol]==='X' || isVisited)
         {
+            
             return false;
         }
         else if(this.grid[pRow][pCol] === 'G')
         {
+            let newVisited = [];
+            for(let i=0;i<visited.length;i++)
+            {
+                newVisited.push(visited[i]);
+            }
+            newVisited.push([pRow,pCol]);
+
+            let rowChange, colChange;
+            for(let i=1;i<newVisited.length;i++)
+            {
+                rowChange = newVisited[i][0] - newVisited[i-1][0];
+                colChange = newVisited[i][1] - newVisited[i-1][1];
+
+                if(colChange === 0)
+                {
+                    switch(rowChange)
+                    {
+                        case 1:
+                            this.solution.push('down');
+                            break;
+                        case -1:
+                            this.solution.push('up');
+                            break;
+                    }
+                }
+                else
+                {
+                    switch(colChange)
+                    {
+                        case 1:
+                            this.solution.push('right');
+                            break;
+                        case -1:
+                            this.solution.push('left');
+                            break;
+                    }
+                }
+            }
+
             return true;
         }
 
@@ -105,19 +145,23 @@ class Maze
         newVisited.push([pRow,pCol]);
 
         let allowedStep = [[-1,0],[1,0],[0,-1],[0,1]];
+        let n = Math.floor(Math.random()*allowedStep.length);
         for(let m=0;m<allowedStep.length;m++)
         {
-            if(this.findPath(pRow+allowedStep[m][0],pCol+allowedStep[m][1],newVisited))
+            if(this.findPath(pRow+allowedStep[n][0],pCol+allowedStep[n][1],newVisited))
             {
+                
                 return true;
             }
-        
+            n = (n+1)%allowedStep.length;
         }
-        return false;
 
+        this.solution.pop();
+        return false;
     }
 }
 
 let maze1 = new Maze(8);
 console.table(maze1.showMaze());
 console.log(maze1.isSolvable());
+console.log(maze1.solution);
